b2cfbb4dd1d670fbc7473b5b701ccf54bef68251
Tony Klinpon <thitiklinpon@gmail.com>
2024-11-21 11:06:14 -0500

    Fix dynamic class on Tailwind

--- src/components/reuseable/button.tsx
+++ src/components/reuseable/button.tsx
@@ -1,14 +1,19 @@
 type Props = {
     text: string
     onClick: () => void
-    color: string
+    color: 'primary' | 'dark'
+}
+
+const colorTheme = {
+    primary: 'bg-primary',
+    dark: 'bg-dark',
 }
 
 const Button = ({ text, onClick, color }: Props) => {
     return (
         <button
             onClick={onClick}
-            className={`rounded bg-${color} p-2 text-sm text-white`}
+            className={`rounded ${colorTheme[color]} p-2 text-sm text-white`}
         >
             {text}
         </button>
